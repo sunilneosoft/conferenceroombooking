3764aecaaa44f15d91621ed6c3a29fa831d306a6
sunilneosoft <sunil.kumar@neosofttech.com>
2016-10-20 20:14:32 +0530

    make better

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.new(first_name: "test", last_name: "", email: "manager@crb.com", password: "password")
+user.skip_confirmation!
+user.save!
+user.add_role :manager
